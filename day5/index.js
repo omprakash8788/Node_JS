648f088e3dfc943c64c34e5962933657906645f2
omprakash8788 <omprakashkumar552@gmail.com>
2024-02-16 08:46:28 +0530

    add

--- day5/index.js
+++ day5/index.js
@@ -1,12 +1,26 @@
 const express=require('express')
 
+
 const app=express()
 
+
+// middleware
+app.use((req,res, next)=>{
+    //logic
+    // console.log("hello from middlware")
+    let startTime=new Date().getTime()
+    next()
+    let endTime=new Date().getTime()
+    console.log(`${endTime-startTime}ms`);
+})
+
 app.get("/",(req,res)=>{
+    console.log('home hu');
     res.send('Home')
 })
 
 app.get("/about",(req,res)=>{
+    console.log('about hu');
     res.send('about')
 })
 
