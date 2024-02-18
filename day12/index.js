4d1bb723cd30e9a1ac798d12e842c348ee864337
omprakash8788 <omprakashkumar552@gmail.com>
2024-02-18 12:13:28 +0530

    add

--- day12/index.js
+++ day12/index.js
@@ -1,12 +1,53 @@
 const express=require('express')
 const {connection}=require('./db')
 const {userRouter}=require('./routes/User.routes')
+const jwt=require('jsonwebtoken')
+
 const app=express()
-
-
 
 app.use(express.json())
 app.use("/users", userRouter)
+
+// create routes
+app.get('/',(req,res)=>{
+    res.send('Home page')
+})
+app.get('/contact',(req,res)=>{
+    res.send('contact page')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('about page')
+})
+// i want protected my movies route
+app.get('/movie',(req,res)=>{
+    // verify a token symmetric
+    const {token}=req.query
+  jwt.verify(token, 'masai',(err, decoded)=> {
+    if(decoded){
+        res.status(200).send("Movie data")
+    }
+    else{
+        res.status(400).send({"err":err.message})
+    }
+  });
+   
+})
+// /series
+app.get('/series',(req,res)=>{
+    // verify a token symmetric
+    const {token}=req.query
+  jwt.verify(token, 'masai',(err, decoded)=> {
+    if(decoded){
+        res.status(200).send("series data")
+    }
+    else{
+        res.status(400).send({"err":err.message})
+    }
+  });
+   
+})
+
 
 app.listen(8080, async()=>{
     try {
--- day12/routes/User.routes.js
+++ day12/routes/User.routes.js
@@ -1,15 +1,48 @@
 const express=require('express')
 const {UserModel}=require('../model/User.model')
+const jwt = require('jsonwebtoken');
+
+
 const userRouter=express.Router()
 
 
-userRouter.post("/register",(req,res)=>{
+userRouter.post("/register",async(req,res)=>{
     // logic
+    const user = new UserModel(req.body)
+    try {
+        await user.save()
+        res.status(200).send({"msg":"New user has been register"})
+    } catch (error) {
+        res.status(400).send({"error":error.messsage})
+
+    }
 })
 
 // login logic
-userRouter.post('/login',(req,res)=>{
+userRouter.post('/login',async(req,res)=>{
     //logic
+    // what data i will need to login
+    // only email, pass
+    const {email, pass}=req.body
+    try {
+        // find email, pass
+      const user= await UserModel.findOne({email,pass})
+    //   console.log(user);
+    //   res.send("..work in progress")
+    if(user){
+        const token = jwt.sign({ course: 'backend' }, 'masai');
+
+        res.status(200).send({"msg":"Login Successfull", "token":token})
+    }
+    else{
+        res.status(200).send({"msg":"Wrong Credentials"})
+
+    }
+        
+    } catch (error) {
+        res.status(400).send({"error":error.messsage})
+        
+    }
 
 })
 
